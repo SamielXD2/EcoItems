203f4f92b47751f68a4fec07c1df7e5c90091272
SamielXD2 <samiellxd@gmail.com>
2026-02-07 15:01:07 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,8 @@ allprojects {
         compileOnly("org.jetbrains:annotations:23.0.0")
         compileOnly("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
         compileOnly("com.github.ben-manes.caffeine:caffeine:3.1.5")
+
+implementation("com.willfp:libreforge:$libreforgeVersion")
     }
 
     java {
